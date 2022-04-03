170a21c9dc28efd9971355453b6b7f0ab4f5e6ec
Annekinnn <102705688+Annekinnn@users.noreply.github.com>
2022-04-03 13:50:10 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,18 +1,18 @@
 
 function selecPrato(pratoClicado) { //pode colocar elemento que nao está no HTML, pois usou o this. só vai usar esse elemento pra add classe no js.
-    const selecao = document.querySelector(".greenborder");
-    if (selecao !== null) { //se tiver algum já selecionado -> retira .greenborder
-      selecao.classList.toggle("greenborder");
+    const pratoselecionado = document.querySelector(".tudo .feed .prato .greenborder");
+    if (pratoselecionado !== null) { //se tiver algum já selecionado -> retira .greenborder
+      pratoselecionado.classList.remove("greenborder");
     }
 
-    pratoClicado.classList.toggle("greenborder"); //faz esse primeiro: adicionar a classe no clicado;
+    pratoClicado.classList.add("greenborder"); //faz esse primeiro: adicionar a classe no clicado;
   }
 
   
 function selecBebida(bebidaClicada) {
-    const selecao = document.querySelector(".greenborder");
-    if (selecao !== null) {
-      selecao.classList.toggle("greenborder");
+    const bebidas = document.querySelector("tudo .feed .bebida .greenborder");
+    if (bebidas !== null) {
+      bebidas.classList.toggle("greenborder");
     }
 
     bebidaClicada.classList.toggle("greenborder");
@@ -20,10 +20,27 @@ function selecBebida(bebidaClicada) {
 
   
 function selecSobre(sobreClicada) {
-    const selecao = document.querySelector(".greenborder");
-    if (selecao !== null) {
-      selecao.classList.toggle("greenborder");
+    const sobremesas = document.querySelector(".tudo .feed .sobremesa .greenborder");
+    if (sobremesas !== null) {
+      sobremesas.classList.toggle("greenborder");
     }
 
     sobreClicada.classList.toggle("greenborder");
   }
+
+
+
+  function fecharPedido(enviar){
+    let opcao1 = document.querySelector(".tudo .feed .prato .greenborder h1").innerHTML;
+    let preco1= document.querySelector(".tudo .feed .prato .greenborder h3").innerHTML;
+
+    let opcao2 =  document.querySelector(".tudo .feed .bebida .greenborder h1").innerHTML;
+    let preco2 = document.querySelector(".tudo .feed .bebida .greenborder h3").innerHTML;
+
+
+    let opcao3 = document.querySelector(".tudo .feed .sobremesa .greenborder h1").innerHTML;
+    let preco3 = document.querySelector(".tudo .feed .sobremesa .greenborder h3").innerHTML;
+
+    
+
+    }
